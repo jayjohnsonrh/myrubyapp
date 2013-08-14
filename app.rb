872f88beeb6472dfc856c4b9ac2914ae9e6c2ab3
Jay Johnson <jajohnso@redhat.com>
2013-08-13 17:30:13 -0700

    showing a change again

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  "CHANGED the time where this server lives is #{Time.now}
+  "CHANGED AGAIN the time where this server lives is #{Time.now}
     <br /><br />check out your <a href=\"/agent\">user_agent</a>"
 end
 
