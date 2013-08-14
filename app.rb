4ea794568f80dd5866d0324e9e63b30e3f4ac033
Jay Johnson <jajohnso@redhat.com>
2013-08-13 18:17:18 -0700

    new commit

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ get '/' do
 <head>
   <meta charset=\"utf-8\">
   <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">
-  <title>Welcome to OpenShift</title>
+  <title>Welcome to OpenShift -- THE DEMO</title>
   <style>
   html {
   background: black;
